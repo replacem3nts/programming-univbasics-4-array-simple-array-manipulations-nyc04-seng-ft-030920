14da9137b79aff9ac89bdbc5f0357c43f6a0d025
Donny Landis <donny.landis@gmail.com>
2020-02-09 20:36:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,13 +10,16 @@ def using_pop(array)
   array.pop
 end
 
-def pop_with_args()
+def pop_with_args(array)
+  array.pop(2)
 end
 
-def using_shift()
+def using_shift(array)
+  array.shift
 end
 
-def shift_with_args()
+def shift_with_args(array)
+  array.shift(2)
 end
 
 def using_concat()
