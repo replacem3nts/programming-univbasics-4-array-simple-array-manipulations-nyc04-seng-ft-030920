71518b90067ea926471335d25b95ccb29b4728ef
Donny Landis <donny.landis@gmail.com>
2020-02-09 20:34:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,3 +5,30 @@ end
 def using_unshift(array, string)
   array.unshift(string)
 end
+
+def using_pop()
+end
+
+def pop_with_args()
+end
+
+def using_shift()
+end
+
+def shift_with_args()
+end
+
+def using_concat()
+end
+
+def using_insert()
+end
+
+def using_uniq()
+end
+
+def using_flatten()
+end
+
+def using_delete()
+end
